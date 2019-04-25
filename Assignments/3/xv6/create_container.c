067e5c43430a8d5856caa04ad3f4017c45b84418
AnshPrakash <anshprakash1997@gmail.com>
2019-04-25 19:04:44 +0530

    some part of COW remaining and schedular is left

--- Assignments/3/xv6/create_container.c
+++ Assignments/3/xv6/create_container.c
@@ -4,6 +4,24 @@
 #include "fs.h"
 #include "fcntl.h"
 
+//convert int to string
+void
+itoa(int num , char* ret)
+{
+  int rem, len = 0;
+  int n = num;
+  while(n!=0){
+    len++;
+    n = n/10;
+  }
+  for(int i=0;i<len;i++){
+    rem = num%10;
+    num = num/10;
+    ret[len - (i+1)] =rem +'0';
+  }
+  ret[len] = '\0';
+}
+
 
 char*
 my_fmtname(char *path)
@@ -45,7 +63,7 @@ my_ls(char *path)
 
   switch(st.type){
   case T_FILE:
-    printf(1, "%s %d %d %d\n", my_fmtname(path), st.type, st.ino, st.size);
+    printf(1, "%s\n", my_fmtname(path));
     break;
 
   case T_DIR:
@@ -65,13 +83,43 @@ my_ls(char *path)
         printf(1, "ls: cannot stat %s\n", buf);
         continue;
       }
-      printf(1, "%s %d %d %d\n", my_fmtname(buf), st.type, st.ino, st.size);
+      printf(1, "%s\n", my_fmtname(buf));
     }
     break;
   }
   close(fd);
 }
 
+void
+destroy_container_wrapper(int cid)
+{
+  char path[4];
+  path[0] = 'c';
+  char  conv[3];
+  itoa(cid,conv);
+  path[1] = conv[0];
+  path[2] = conv[1];
+  path[3] = '\0';
+
+  
+  char deldir[7];
+  deldir[0] = '.';
+  deldir[1] = '.';
+  deldir[2] = '/';
+  for(int i=0 ;i<4;i++){
+    deldir[3+i] = path[i];
+  }
+
+  unlink(deldir);
+  mkdir(path);
+  destroy_container(cid);
+}
+
+void
+cont_ls()
+{
+  my_ls(".");
+}
 
 void
 init_containers_resources(){
@@ -99,23 +147,6 @@ init_containers_resources(){
 }
 
 
-//convert int to string
-void
-itoa(int num , char* ret)
-{
-  int rem, len = 0;
-  int n = num;
-  while(n!=0){
-    len++;
-    n = n/10;
-  }
-  for(int i=0;i<len;i++){
-    rem = num%10;
-    num = num/10;
-    ret[len - (i+1)] =rem +'0';
-  }
-  ret[len] = '\0';
-}
 
 void
 join_container_wrapper(int i){
@@ -128,6 +159,13 @@ join_container_wrapper(int i){
   cid_s[3] = '\0';
   chdir(cid_s);
   join_container(i);
+}
+
+int
+leave_container_wrapper()
+{
+  chdir("/");
+  return(leave_container());
 }
 
 void 
@@ -171,28 +209,14 @@ int main(void)
 
   init_containers_resources();
 	create_container(1);
+  create_container(2);
+  
 
-  // int fd;
-  // if((fd = open("hello",O_CREATE|O_RDWR))<0){
-  //   printf(1," not able to create the file\n");
-  // }
-  // char raw[5] = "ansh\n";
-  // int n =5;
-  // write(fd,raw,n);
-  // close(fd);
-  // char r[3];
-  // itoa(1,r);
-  // printf(0,"%s\n",r);
-  // my_ls(".");
-	// create_container(2);
-	// create_container(3);
-	// newls();
-	
-	// destroy_container(1);
-	int pid = fork();
-	if(pid == 0){
-		join_container_wrapper(1); // called only by child created by preceeding fork call .
-		// fork();
+
+  //////////////FILE ISOLATION///////////////////////
+  int pid = fork();
+  if(pid == 0){
+    join_container_wrapper(1); // called only by child created by preceeding fork call .
     create_file("hello");
     int fd;
     fd =open_file("hello",2);
@@ -200,11 +224,32 @@ int main(void)
     int n =5;
     write(fd,raw,n);
     close(fd);
-    my_ls(".");
-		// ps();
-		// leave_container();
-		// ps();		
-	}
+    printf(0,"Testing ls \n\n");
+    cont_ls();
+    // printf(0,"Testing ps \n\n\n");
+    // ps();
+    exit();
+  }
+  // wait();
+	destroy_container_wrapper(1);
+  // pid = fork();
+  // if(pid == 0){
+  //   join_container_wrapper(2); // called only by child created by preceeding fork call .
+  //   create_file("YOYO");
+  //   int fd;
+  //   fd =open_file("YOYO",2);
+  //   char raw[5] = "five\n";
+  //   int n =5;
+  //   write(fd,raw,n);
+  //   close(fd);
+  //   printf(0,"Testing ls \n\n\n");
+  //   cont_ls();
+  //   printf(0,"Testing ps \n\n\n");
+  //   ps();
+  //   exit();
+  // }
+  /////////////////////////////
+
 
 	// fork();
 	// join_container(1); // called only by child created by preceeding fork call .
@@ -220,6 +265,6 @@ int main(void)
 	// ps() ;
 	// while(1);
 	wait();
-	exit();
+  exit();
 }
 
